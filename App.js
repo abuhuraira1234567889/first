f53510403a9fc2d58e7736e18ef538565103db5a
abuhuraira1234567889 <hurairaabu098@gmail.com>
2022-03-22 12:04:04 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Alert} from 'react-native';
+
+export default function App() {
+  function dun2(){
+    
+    console.log("ok")
+    return dun2
+    
+  }
+//   const fun=()=>{
+//     // program to shuffle the deck of cards
+
+// // declare card elements
+// const suits = ["Spades", "Diamonds", "Club", "Heart"];
+// const values = [
+//   "Ace",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "10",
+//   "Jack",
+//   "Queen",
+//   "King",
+// ];
+
+// // empty array to contain cards
+// let deck = [];
+
+// // create a deck of cards
+// for (let i = 0; i < suits.length; i++) {
+//     for (let x = 0; x < values.length; x++) {
+//         let card = { Value: values[x], Suit: suits[i] };
+//         deck.push(card);
+//     }
+// }
+
+// // shuffle the cards
+// for (let i = deck.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * i);
+//     let temp = deck[i];
+//     deck[i] = deck[j];
+//     deck[j] = temp;
+// }
+
+// console.log('The first five cards are:');
+
+// // display 5 results
+// for (let i = 0; i < 5; i++) {
+//     console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+// }
+// return fun
+//   }
+
+
+  
+  return (
+    
+      dun2()
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#dfdfdf',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
